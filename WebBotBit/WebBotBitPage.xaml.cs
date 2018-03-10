47a7f186df5ed35dc22747334e74664251440b7f
June <junecho.ucb@gmail.com>
2018-03-10 15:57:25 -0800

    Initial commit

---
+++ WebBotBit/WebBotBitPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WebBotBit
+{
+    public partial class WebBotBitPage : ContentPage
+    {
+        public WebBotBitPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
