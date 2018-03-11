bd71012b386262fb8dd62f88c4c412f9117212c2
June <junecho.ucb@gmail.com>
2018-03-10 16:13:52 -0800

    Added BotPaGE

---
+++ WebBotBit/Pages/BotPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WebBotBit.Pages
+{
+    public partial class BotPage : ContentPage
+    {
+        public BotPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
